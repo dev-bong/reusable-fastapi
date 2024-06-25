3579db3c49c4fbda79540f1effca8c98fb435367
dev-bong <qhddnjswo011@gmail.com>
2024-06-26 00:31:05 +0900

    ✅ 비밀번호 검증 및 JWT 토큰 발급 기능 추가

--- app/core/security.py
+++ app/core/security.py
@@ -1,7 +1,25 @@
+from datetime import datetime, timedelta
+
 from passlib.context import CryptContext
+import jwt
+
+from app.core.config import settings
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 def get_password_hash(password: str) -> str:
     return pwd_context.hash(password)
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def create_access_token(sub: str) -> str:
+    exp = datetime.now() + timedelta(minutes=settings.JWT_TOKEN_EXP)
+    jwt_form = {"sub": sub, "exp": exp}
+    encoded_jwt = jwt.encode(
+        payload=jwt_form, key=settings.SECRET_KEY, algorithm=settings.JWT_ALGORITHM
+    )
+    return encoded_jwt
