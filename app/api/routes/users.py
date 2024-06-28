68febf97378905bb8597d950974f3814b8f32247
dev-bong <qhddnjswo011@gmail.com>
2024-06-28 13:12:09 +0900

    ♻️ 현재 유저 조회 의존성 주입 코드로 변경
    - JWT 토큰을 받아 현재 유저 정보를 조회하는 부분을 의존성 주입 코드로 변경
    - 현재 유저 정보가 필요하거나 JWT 인증을 필요로 하는 api에서 사용 가능

--- app/api/routes/users.py
+++ app/api/routes/users.py
@@ -5,8 +5,7 @@ from starlette import status
 
 from app.schemas import user_schema
 from app.crud import user_crud
-from app.core import security
-from app.api.deps import SessionDep, TokenDep
+from app.api.deps import SessionDep, CurrentUser
 
 router = APIRouter()
 
@@ -35,16 +34,5 @@ def create_user(session: SessionDep, user_info: user_schema.UserCreate) -> Any:
     summary="현재 회원 정보 조회",
     description="현재 로그인 되어있는 회원의 정보 조회",
 )
-def read_user_me(session: SessionDep, token: TokenDep) -> Any:
-    payload = security.decode_token(token=token.credentials)
-    if not payload:
-        raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN, detail="잘못된 토큰입니다."
-        )
-
-    current_user = user_crud.get_user_by_id(session=session, id=payload["sub"])
-    if not current_user:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="존재하지 않는 유저입니다."
-        )
+def read_user_me(current_user: CurrentUser) -> Any:
     return current_user
